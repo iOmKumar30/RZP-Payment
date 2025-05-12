9fe63b8036290ee5e10b5057b110a878a4d8bf2c
iOmKumar30 <iomkumar03@outlook.com>
2025-05-12 17:21:56 +0530

    Frontend URL CORS

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://rzp-payment.vercel.app/",
+    credentials: true
+}));
 app.use(express.json());
 
 const paymentRoutes = require("./routes/payment");
